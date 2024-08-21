21ff41708bcab1c4da3a00fb922bb831c9c9d075
Abhijit Mondal <abhijitmondal.cse@gmail.com>
2024-08-21 13:50:23 +0600

    Code Refactor

--- Server-Side/server.js
+++ Server-Side/server.js
@@ -25,7 +25,7 @@ mongoose.connect(DB, {}).then((con) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
 
