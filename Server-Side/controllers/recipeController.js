9ffa39a20284c10e7c34b61517f750c11dbfee62
Abhijit Mondal <abhijitmondal.cse@gmail.com>
2024-08-23 18:14:48 +0600

    Code Refactor

--- Server-Side/controllers/recipeController.js
+++ Server-Side/controllers/recipeController.js
@@ -1,9 +1,8 @@
 const Recipe = require("./../models/recipeModel");
 
 exports.createRecipe = async (req, res, next) => {
-  const recipe = await Recipe.create(req.body);
-
   try {
+    const recipe = await Recipe.create(req.body);
     res.status(200).json({
       status: "success",
       data: {
@@ -19,9 +18,8 @@ exports.createRecipe = async (req, res, next) => {
 };
 
 exports.getAllRecipes = async (req, res, next) => {
-  const recipes = await Recipe.find({});
-
   try {
+    const recipes = await Recipe.find({});
     res.status(200).json({
       status: "success",
       results: recipes.length,
@@ -38,17 +36,24 @@ exports.getAllRecipes = async (req, res, next) => {
 };
 
 exports.deleteRecipe = async (req, res, next) => {
-  const recipe = await Recipe.findByIdAndDelete(req.params.id);
+  try {
+    const recipe = await Recipe.findByIdAndDelete(req.params.id);
 
-  if (!recipe) {
-    return res.status(404).json({
+    if (!recipe) {
+      return res.status(404).json({
+        status: "error",
+        message: "Recipe not found",
+      });
+    }
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
       status: "error",
-      message: "Recipe not found",
+      message: err.message,
     });
   }
-
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
 };
